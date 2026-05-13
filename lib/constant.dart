1e832522eedd3eea30df7560fd5672dd44e051b3
mohamed alaa <muhamedawadkassem@gmail.com>
2026-05-13 15:21:42 +0300

    Update constant.dart

--- lib/constant.dart
+++ lib/constant.dart
@@ -9,6 +9,6 @@ const kCreatedAt = 'createdAt';
 
 String generateChatId(String email1, String email2) {
   List<String> emails = [email1, email2];
-  emails.sort(); // الترتيب الأبجدي هو السر
+  emails.sort();  
   return "${emails[0]}_${emails[1]}";
 }

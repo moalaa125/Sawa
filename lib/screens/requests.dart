45ce3917b0eb58c064a9a8976fa73eb0a173dedb
moalaa125 <muhamedawadkassem@gmail.com>
2026-05-30 18:54:34 +0300

    build the UI for requests screen

--- lib/screens/requests.dart
+++ lib/screens/requests.dart
@@ -6,8 +6,33 @@ class Requests extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Requests'),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 20.0),
+        children: [
+          Center(
+            child: Container(
+              height: 80,
+              width: 400,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.grey[300],
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+
+                children: [
+                  Text('lolmohamedalaajj@gmail.com'),
+                  SizedBox(width: 40),
+
+                  Icon(Icons.check, color: Colors.green),
+                  SizedBox(width: 10),
+
+                  Icon(Icons.close, color: Colors.red),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }

a07f4e5b685c31dc4c4a0a724bc5ac06f0364013
moalaa125 <muhamedawadkassem@gmail.com>
2026-05-30 17:30:07 +0300

    add nav bar , create a profile , send_request , request screens

---
+++ lib/screens/requests.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Requests extends StatelessWidget {
+  const Requests({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Requests'),
+      ),
+    );
+  }
+}
--- lib/screens/users.dart
+++ lib/screens/users.dart
@@ -1,6 +1,9 @@
 import 'package:chat_app/constant.dart';
 import 'package:chat_app/custom_widgets/app_router.dart';
 import 'package:chat_app/screens/chat_screen.dart';
+import 'package:chat_app/screens/profile.dart';
+import 'package:chat_app/screens/requests.dart';
+import 'package:chat_app/screens/send_request.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
@@ -8,17 +11,48 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:chat_app/screens/login.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
-class UsersScreen extends StatelessWidget {
+class UsersScreen extends StatefulWidget {
   static String id = 'usersScreen';
 
-  const UsersScreen({super.key});
+  UsersScreen({super.key});
+
+  @override
+  State<UsersScreen> createState() => _UsersScreenState();
+}
+
+class _UsersScreenState extends State<UsersScreen> {
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     final String? myEmail = FirebaseAuth.instance.currentUser?.email;
 
     return Scaffold(
+      bottomNavigationBar: GNav(
+        rippleColor: Colors.grey[300]!,
+        hoverColor: Colors.grey[100]!,
+        gap: 4,
+        activeColor: kSecoundColor,
+        iconSize: 24.sp,
+        duration: Duration(milliseconds: 400),
+        color: Colors.black,
+        tabBackgroundColor: Colors.grey[200]!,
+        backgroundColor: Colors.white,
+        tabs: [
+          GButton(icon: Icons.home, text: 'Home', onPressed: () {}),
+          GButton(icon: Icons.search, text: 'Search', onPressed: () {}),
+          GButton(icon: Icons.request_page, text: 'Requests'),
+          GButton(icon: Icons.person, text: 'Profile'),
+        ],
+        selectedIndex: _currentIndex,
+        onTabChange: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
       backgroundColor: Colors.white,
       appBar: AppBar(
         actions: [
@@ -46,90 +80,97 @@ class UsersScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance.collection('users').snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            var users = snapshot.data!.docs.where((doc) {
-              return doc['email'] != myEmail;
-            }).toList();
 
-            if (users.isEmpty) {
-              return Center(child: Text("No other users found."));
-            }
+      body: _getSelectedPage(myEmail),
+    );
+  }
 
-            return ListView.builder(
-              itemCount: users.length,
-              itemBuilder: (context, index) {
-                var userData = users[index].data() as Map<String, dynamic>;
+  Widget _getSelectedPage(String? myEmail) {
+    if (_currentIndex == 1) {
+      return SendRequest();
+    } else if (_currentIndex == 2) {
+      return Requests();
+    } else if (_currentIndex == 3) {
+      return Profile();
+    }
 
-                String otherUserEmail = userData['email'] ?? "No Email";
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection('users').snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          var users = snapshot.data!.docs.where((doc) {
+            return doc['email'] != myEmail;
+          }).toList();
 
-                String displayName = userData.containsKey('userName')
-                    ? userData['userName']
-                    : otherUserEmail.split('@')[0];
-
-                return Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 16.w,
-                    vertical: 8.h,
-                  ),
-                  child: Card(
-                    elevation: 0,
-                    color: const Color(0xFFF0F0F0),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15.r),
-                    ),
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        backgroundColor: kSecoundColor,
-                        child: Text(
-                          displayName[0].toUpperCase(),
-                          style: const TextStyle(color: Colors.white),
-                        ),
-                      ),
-                      title: Text(
-                        displayName,
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: kSecoundColor,
-                        ),
-                      ),
-                      subtitle: Text(otherUserEmail),
-                      trailing: Icon(
-                        Icons.send_rounded,
-                        color: kSecoundColor,
-                        size: 20.sp,
-                      ),
-                      onTap: () {
-                        String roomId = generateChatId(
-                          myEmail!,
-                          otherUserEmail,
-                        );
-
-                        Navigator.of(context).push(
-                          sharedAxisRoute(
-                            ChatScreen(),
-                            arguments: {
-                              'email': myEmail,
-                              'roomId': roomId,
-                              'otherUserName': displayName,
-                            },
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                );
-              },
-            );
-          } else if (snapshot.hasError) {
-            return const Center(child: Text("Something went wrong"));
-          } else {
-            return Center(child: SpinKitWave(color: kSecoundColor, size: 50.0));
+          if (users.isEmpty) {
+            return Center(child: Text("No other users found."));
           }
-        },
-      ),
+
+          return ListView.builder(
+            itemCount: users.length,
+            itemBuilder: (context, index) {
+              var userData = users[index].data() as Map<String, dynamic>;
+
+              String otherUserEmail = userData['email'] ?? "No Email";
+
+              String displayName = userData.containsKey('userName')
+                  ? userData['userName']
+                  : otherUserEmail.split('@')[0];
+
+              return Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 8.h),
+                child: Card(
+                  elevation: 0,
+                  color: const Color(0xFFF0F0F0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.r),
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: kSecoundColor,
+                      child: Text(
+                        displayName[0].toUpperCase(),
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    title: Text(
+                      displayName,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: kSecoundColor,
+                      ),
+                    ),
+                    subtitle: Text(otherUserEmail),
+                    trailing: Icon(
+                      Icons.send_rounded,
+                      color: kSecoundColor,
+                      size: 20.sp,
+                    ),
+                    onTap: () {
+                      String roomId = generateChatId(myEmail!, otherUserEmail);
+
+                      Navigator.of(context).push(
+                        sharedAxisRoute(
+                          ChatScreen(),
+                          arguments: {
+                            'email': myEmail,
+                            'roomId': roomId,
+                            'otherUserName': displayName,
+                          },
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              );
+            },
+          );
+        } else if (snapshot.hasError) {
+          return const Center(child: Text("Something went wrong"));
+        } else {
+          return Center(child: SpinKitWave(color: kSecoundColor, size: 50.0));
+        }
+      },
     );
   }
 }

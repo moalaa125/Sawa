972c9eb18a406f8a87f514c247785dbfee849ea8
moalaa125 <muhamedawadkassem@gmail.com>
2026-06-05 16:13:25 +0300

    update the verificatio screen

--- lib/screens/verification_screen.dart
+++ lib/screens/verification_screen.dart
@@ -63,7 +63,7 @@ class _VerificationScreenState extends State<VerificationScreen> {
       backgroundColor: kPrimaryColor,
 
       appBar: AppBar(
-        backgroundColor: Colors.white,
+        backgroundColor: kAppBarColor,
         elevation: 0,
         surfaceTintColor: Colors.transparent,
         centerTitle: true,
@@ -71,100 +71,105 @@ class _VerificationScreenState extends State<VerificationScreen> {
         title: Material(
           color: Colors.transparent,
           child: Text(
-            'SAWA Chat',
+            'SAWA',
             style: TextStyle(
               color: kSecoundColor,
               fontSize: 25.sp,
-              fontFamily: 'Pacifico',
+              fontFamily: 'amara',
             ),
           ),
         ),
       ),
 
       body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-
-            children: [
-              Column(
-                children: [
-                  Container(
-                    height: 110.h,
-                    width: 110.w,
-
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: const Color(0xFFF4F3F7),
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(gradient: kPrimaryGradient),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      height: 110.h,
+                      width: 110.w,
+          
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: const Color(0xFFF4F3F7),
+                      ),
+          
+                      child: Icon(
+                        CupertinoIcons.mail,
+                        color: kSecoundColor,
+                        size: 40.sp,
+                      ),
                     ),
-
-                    child: Icon(
-                      CupertinoIcons.mail,
-                      color: kSecoundColor,
-                      size: 40.sp,
+          
+                    SizedBox(height: 35.h),
+          
+                    Text(
+                      'Check your inbox\n',
+                      textAlign: TextAlign.center,
+          
+                      style: TextStyle(
+                        color: kSecoundColor,
+                        fontSize: 34.sp,
+                        fontFamily: 'amara',
+                      ),
                     ),
-                  ),
-
-                  SizedBox(height: 35.h),
-
-                  Text(
-                    'Check your inbox\n',
-                    textAlign: TextAlign.center,
-
-                    style: TextStyle(
-                      color: kSecoundColor,
-                      fontSize: 34.sp,
-                      fontFamily: 'amara',
+          
+                    Text(
+                      'We sent a verification link to your email.\n Please click the link to confirm\n your account and continue\n to SAWA Chat.',
+                      textAlign: TextAlign.center,
+          
+                      style: TextStyle(
+                        color: kSecoundColor.withValues(alpha: .6),
+                        fontSize: 16.sp,
+                        height: 1.6,
+                      ),
                     ),
-                  ),
-
-                  Text(
-                    'We sent a verification link to your email.\n Please click the link to confirm\n your account and continue\n to SAWA Chat.',
-                    textAlign: TextAlign.center,
-
-                    style: TextStyle(
-                      color: kSecoundColor.withOpacity(0.6),
-                      fontSize: 16.sp,
-                      height: 1.6,
-                    ),
-                  ),
-                ],
-              ),
-
-              Column(
-                children: [
-                  isLoading
-                      ? SizedBox(
-                          height: 56.h,
-
-                          child: Center(
-                            child: CustomLoadingIndicator(),
+                  ],
+                ),
+          
+                Column(
+                  children: [
+                    isLoading
+                        ? SizedBox(
+                            height: 56.h,
+          
+                            child: Center(
+                              child: CustomLoadingIndicator(),
+                            ),
+                          )
+                        : CustomButton(
+                            ontap: () {
+                              isTheUserVerified();
+                            },
+          
+                            text: "I've verified, continue",
                           ),
-                        )
-                      : CustomButton(
-                          ontap: () {
-                            isTheUserVerified();
-                          },
-
-                          text: "I've verified, continue",
-                        ),
-
-                  SizedBox(height: 5.h),
-
-                  CustomTextButton(
-                    onPressed: () {
-                      resendLink();
-                    },
-
-                    text: 'RESEND LINK',
-
-                    textStyle: TextStyle(color: kSecoundColor, fontSize: 16.sp),
-                  ),
-
-                  SizedBox(height: 30.h),
-                ],
-              ),
-            ],
+          
+                    SizedBox(height: 5.h),
+          
+                    CustomTextButton(
+                      onPressed: () {
+                        resendLink();
+                      },
+          
+                      text: 'RESEND LINK',
+          
+                      textStyle: TextStyle(color: kSecoundColor, fontSize: 16.sp),
+                    ),
+          
+                    SizedBox(height: 30.h),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
